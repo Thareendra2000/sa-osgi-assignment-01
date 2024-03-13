e81d6124e6d07b7023c232ecb24723adf724ea02
Thareendra2000 <thareendra2000@gmail.com>
2024-03-14 00:32:04 +0530

    changes added to membershipPublisher

--- membershipPublisher/src/membershippublisher/Activator.java
+++ membershipPublisher/src/membershippublisher/Activator.java
@@ -1,18 +1,27 @@
 package membershippublisher;
 
+import membershippublisher.membershipImpl;
+import membershippublisher.membershipInterface;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
 
 public class Activator implements BundleActivator {
 
-	@Override
-	public void start(BundleContext context) throws Exception {
-		System.out.println("Hello World!!");
-	}
-	
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		System.out.println("Goodbye World!!");
-	}
+    private ServiceRegistration membershipServiceRegistration;
 
+    public void start(BundleContext context) throws Exception {
+        System.out.println("Membership service up and running");
+
+        // Register Membership service
+        membershipInterface membershipInterface = new membershipImpl();
+        membershipServiceRegistration = context.registerService(membershipInterface.class.getName(), membershipInterface, null);
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        System.out.println("Membership service shut down");
+        membershipServiceRegistration.unregister();
+    }
 }
--- membershipPublisher/src/membershippublisher/membershipImpl.java
+++ membershipPublisher/src/membershippublisher/membershipImpl.java
@@ -1,7 +1,5 @@
 package membershippublisher;
 
-
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
